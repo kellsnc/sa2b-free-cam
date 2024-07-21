ecc580532a2e4c0cd86ef6966adc0f2a2337b4a4
Kell <kellspam@outlook.fr>
2024-07-21 03:19:34 +0200

    Compatibility with Chao Data Menu

--- sa2b-free-cam/menu.cpp
+++ sa2b-free-cam/menu.cpp
@@ -9,10 +9,12 @@ enum PauseModes
     PauseMode_Closed,
     PauseMode_Main,
     PauseMode_Help,
-    PauseMode_Camera // added
+    PauseMode_ChaoData, // Added by Justin for their Chao Data mod
+    PauseMode_Camera // Added for this mod
 };
 
 Trampoline* GameStateHandler_t = nullptr;
+Trampoline* PauseMenuDisplay_t = nullptr;
 Trampoline* InitPauseMenu_t = nullptr;
 Trampoline* FreePauseMenu_t = nullptr;
 
@@ -53,60 +55,26 @@ static void* CameraMenuFontTextures[] { nullptr, nullptr };
 // Rewrite the pause menu display to add the camera submenu display
 static void __cdecl PauseMenuDisplay_r()
 {
-    ScreenDrawMode = 0;
-    njDisableFog();
-    
-    RenferInfo_->unknown1 = RenferInfo_->unknown1 & 0x3FFFFFF | 0x94000000;
-    if (*(int*)0x1A558B8 == 1 || *(int*)0x1A558BC == 1)
+    if (!(*(int*)0x1A558B8 == 1 || *(int*)0x1A558BC == 1) && PauseMode == PauseMode_Camera)
     {
-        if (*(int*)0x1A558BC)
+        ScreenDrawMode = 0;
+        njDisableFog();
+
+		RenferInfo_->unknown1 = RenferInfo_->unknown1 & ~0xFC000000 | 0x94000000;
+        
+        DrawSpriteThing(*(void**)0x1A55928, 168.0f, 180.0f, 320.0f, 164.0f, 0.99f, 0.0f, 0.0f, 1.0f, 1.0f, -1);
+        for (int i = 0; i < PauseOptionCount; ++i)
         {
-            DrawPauseMassage(MenuButtons_SomethingWasPressed, *(void**)0x1A558F0);
+            DrawPauseText(CameraMenuFontTextures[i], i, i);
         }
-        if (*(int*)0x1A558B8)
-        {
-            DrawPauseMassage(MenuButtons_SomethingWasPressed, *(void**)0x1A558EC);
-        }
+
+        ScreenDrawMode = 1;
+        njEnableFog();
     }
     else
     {
-        if (PauseMode == PauseMode_Main)
-        {
-            DrawPauseBackground(PauseOptionCount);
-            for (int i = 0; i < PauseOptionCount; ++i)
-            {
-                void* v3;
-                if (i >= 1 && PauseOptionCount == 5)
-                {
-                    v3 = PauseMenuMainFontTexs[i + 1];
-                }
-                else
-                {
-                    v3 = PauseMenuMainFontTexs[i];
-                }
-                DrawPauseText(v3, i, i);
-            }
-        }
-        else if (PauseMode == PauseMode_Help)              // Help menu
-        {
-            DrawPauseBackground(PauseOptionCount);
-            for (int j = 0; j < PauseOptionCount; ++j)
-            {
-                DrawPauseText(PauseMenuHelpFontTexs[j], j, j);
-            }
-        }
-        else if (PauseMode == PauseMode_Camera)
-        {
-            DrawSpriteThing(*(void**)0x1A55928, 168.0f, 180.0f, 320.0f, 164.0f, 0.99f, 0.0f, 0.0f, 1.0f, 1.0f, -1);
-            for (int i = 0; i < PauseOptionCount; ++i)
-            {
-                DrawPauseText(CameraMenuFontTextures[i], i, i);
-            }
-        }
+        TARGET_DYNAMIC(PauseMenuDisplay)();
     }
-
-    ScreenDrawMode = 1;
-    njEnableFog();
 }
 
 static void ReturnToMainPauseMenu()
@@ -114,7 +82,7 @@ static void ReturnToMainPauseMenu()
     PlaySoundProbably(32770, 0, 0, 0);
     PauseMode = PauseMode_Main;
     PauseOptionCount = (!TwoPlayerMode && Life_Count[0] > 0) ? 6 : 5;
-    PauseSelection = PauseOptionCount == 6 ? 2 : 1;
+    PauseSelection = PauseOptionCount == 6 ? 3 : 2;
     ControllersRaw[0].press = 0; // prevent original function from running wrong code
 }
 
@@ -130,12 +98,12 @@ static void GoToCameraMenu()
 // Hook the gamestate handler to add a submenu to the pause menu
 static BOOL __cdecl GameStateHandler_r()
 {
-    if (GameState == GameStates_Pause)
+    if (GameState == GameStates_Pause && CurrentLevel != LevelIDs_ChaoWorld)
     {
         // Main menu logic override:
         if (PauseMode == PauseMode_Main && ControllersRaw[0].press & Buttons_A)
         {
-            if ((PauseSelection == 2 && PauseOptionCount == 6) || (PauseSelection == 1 && PauseOptionCount == 5))
+            if ((PauseSelection == 3 && PauseOptionCount == 6) || (PauseSelection == 2 && PauseOptionCount == 5))
             {
                 GoToCameraMenu();
             }
@@ -182,16 +150,15 @@ static BOOL __cdecl FreePauseMenu_r()
 
 void InitMenu()
 {   
-    PauseMenuMainTextsJP[2] = CameraModeTexts[Language_Japanese];
-    PauseMenuMainTextsUS[2] = CameraModeTexts[Language_English];
-    PauseMenuMainTextsFR[2] = CameraModeTexts[Language_French];
-    PauseMenuMainTextsES[2] = CameraModeTexts[Language_Spanish];
-    PauseMenuMainTextsGE[2] = CameraModeTexts[Language_German];
-    PauseMenuMainTextsIT[2] = CameraModeTexts[Language_Italian];
-
-    WriteJump((void*)0x440AD0, PauseMenuDisplay_r);
+    PauseMenuMainTextsJP[3] = CameraModeTexts[Language_Japanese];
+    PauseMenuMainTextsUS[3] = CameraModeTexts[Language_English];
+    PauseMenuMainTextsFR[3] = CameraModeTexts[Language_French];
+    PauseMenuMainTextsES[3] = CameraModeTexts[Language_Spanish];
+    PauseMenuMainTextsGE[3] = CameraModeTexts[Language_German];
+    PauseMenuMainTextsIT[3] = CameraModeTexts[Language_Italian];
 
     GameStateHandler_t = new Trampoline(0x43A780, 0x43A786, GameStateHandler_r);
+    PauseMenuDisplay_t = new Trampoline(0x440AD0, 0x440AD8, PauseMenuDisplay_r);
     InitPauseMenu_t = new Trampoline(0x439610, 0x439616, InitPauseMenu_r);
     FreePauseMenu_t = new Trampoline(0x439C80, 0x439C85, FreePauseMenu_r);
 }
